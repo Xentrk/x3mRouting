2fe74aa33567c413fb13d9125c53a84622b94702
Xentrk <xentrk@x3mtek.com>
2019-04-03 13:00:15 +0700

    updated lockfile code

--- load_DNSMASQ_ipset.sh
+++ load_DNSMASQ_ipset.sh
@@ -27,171 +27,168 @@ logger -st "($(basename "$0"))" $$ Starting Script Execution
 # Uncomment the line below for debugging
 set -x
 
-PROGNAME=$(basename "$0")
-LOCKFILE_DIR=/tmp
-LOCK_FD=200
-
-lock() {
-    local prefix=$1
-    local fd=${2:-$LOCK_FD}
-    local lock_file=$LOCKFILE_DIR/$prefix.lock
-
-    # create lock file
-    eval "exec $fd>$lock_file"
-
-    # acquier the lock
-    flock -n "$fd" \
-        && return 0 \
-        || return 1
+Kill_Lock() {
+  if [ -f "/tmp/load_DNSMASQ_ipset.lock" ] && [ -d "/proc/$(sed -n '2p' /tmp/load_DNSMASQ_ipset.lock)" ]; then
+    logger -st "($(basename "$0"))" "[*] Killing Locked Processes ($(sed -n '1p' /tmp/load_DNSMASQ_ipset.lock)) (pid=$(sed -n '2p' /tmp/load_DNSMASQ_ipset.lock))"
+    logger -st "($(basename "$0"))" "[*] $(ps | awk -v pid="$(sed -n '2p' /tmp/load_DNSMASQ_ipset.lock)" '$1 == pid')"
+    kill "$(sed -n '2p' /tmp/load_DNSMASQ_ipset.lock)"
+    rm -rf /tmp/load_DNSMASQ_ipset.lock
+    echo
+  fi
 }
 
-error_exit() {
-    error_str="$@"
-    logger -t "($(basename "$0"))" $$ "$error_str"
-    exit 1
-}
-
-main() {
-    lock "$PROGNAME" || error_exit "Exiting $PROGNAME. Only one instance of $PROGNAME can run at one time."
-
-# Chk_Entware function provided by @Martineau at snbforums.com
-
-Chk_Entware () {
-
-    # ARGS [wait attempts] [specific_entware_utility]
-
-    local READY=1                  # Assume Entware Utilities are NOT available
-    local ENTWARE="opkg"
-    ENTWARE_UTILITY=                # Specific Entware utility to search for (Tacky GLOBAL variable returned!)
-
-    local MAX_TRIES=30
-    if [ -n "$2" ] && [ -n "$(echo $2 | grep -E '^[0-9]+$')" ];then
-        local MAX_TRIES=$2
-    fi
-
-    if [ -n "$1" ] && [ -z "$(echo $1 | grep -E '^[0-9]+$')" ];then
-        ENTWARE_UTILITY=$1
+Check_Lock() {
+  if [ -f "/tmp/load_DNSMASQ_ipset.lock" ] && [ -d "/proc/$(sed -n '2p' /tmp/load_DNSMASQ_ipset.lock)" ] && [ "$(sed -n '2p' /tmp/load_DNSMASQ_ipset.lock)" != "$$" ]; then
+    if [ "$(($(date +%s) - $(sed -n '3p' /tmp/load_DNSMASQ_ipset.lock)))" -gt "1800" ]; then
+      Kill_Lock
     else
-        if [ -z "$2" ] && [ -n "$(echo $1 | grep -E '^[0-9]+$')" ];then
-            MAX_TRIES=$1
-        fi
+      logger -st "($(basename "$0"))" "[*] Lock File Detected ($(sed -n '1p' /tmp/load_DNSMASQ_ipset.lock)) (pid=$(sed -n '2p' /tmp/load_DNSMASQ_ipset.lock)) - Exiting (cpid=$$)"
+      echo
+      exit 1
     fi
+  fi
+  echo "$@" >/tmp/load_DNSMASQ_ipset.lock
+  echo "$$" >>/tmp/load_DNSMASQ_ipset.lock
+  date +%s >>/tmp/load_DNSMASQ_ipset.lock
+  lock_load_DNSMASQ_ipset="true"
+}
 
-   # Wait up to (default) 30 seconds to see if Entware utilities available.....
-   local TRIES=0
-   while [ $TRIES -lt $MAX_TRIES ];do
-      if [ -n "$(which $ENTWARE)" ] && [ "$($ENTWARE -v | grep -o "version")" == "version" ];then		# Check Entware exists and it executes OK
-         if [ -n "$ENTWARE_UTILITY" ];then      								# Specific Entware utility installed?
-            if [ -n "$($ENTWARE list-installed $ENTWARE_UTILITY)" ];then
-                READY=0                                                         # Specific Entware utility found
-            else
-                # Not all Entware utilities exist as a stand-alone package e.g. 'find' is in package 'findutils'
-				# 	opkg files findutils
-				#
-				# 	Package findutils (4.6.0-1) is installed on root and has the following files:
-				# 	/opt/bin/xargs
-				# 	/opt/bin/find
-				# Add 'executable' as 'stubby' leaves behind two directories containing the string 'stubby'
-				if [ -d /opt ] && [ -n "$(find /opt/ -type f -executable -name $ENTWARE_UTILITY)" ];then
-					READY=0														# Specific Entware utility found
-				fi
-            fi
-         else
-            READY=0                                                             # Entware utilities ready
-         fi
-         break
+Chk_Entware() {
+
+  # ARGS [wait attempts] [specific_entware_utility]
+
+  READY=1 # Assume Entware Utilities are NOT available
+  ENTWARE="opkg"
+  ENTWARE_UTILITY= # Specific Entware utility to search for (Tacky GLOBAL variable returned!)
+
+  MAX_TRIES=30
+  if [ -n "$2" ] && [ -n "$(echo $2 | grep -E '^[0-9]+$')" ]; then
+    MAX_TRIES=$2
+  fi
+
+  if [ -n "$1" ] && [ -z "$(echo $1 | grep -E '^[0-9]+$')" ]; then
+    ENTWARE_UTILITY=$1
+  else
+    if [ -z "$2" ] && [ -n "$(echo $1 | grep -E '^[0-9]+$')" ]; then
+      MAX_TRIES=$1
+    fi
+  fi
+
+  # Wait up to (default) 30 seconds to see if Entware utilities available.....
+  TRIES=0
+  while [ "$TRIES" -lt "$MAX_TRIES" ]; do
+    if [ -n "$(which $ENTWARE)" ] && [ "$($ENTWARE -v | grep -o "version")" = "version" ]; then # Check Entware exists and it executes OK
+      if [ -n "$ENTWARE_UTILITY" ]; then # Specific Entware utility installed?
+        if [ -n "$($ENTWARE list-installed $ENTWARE_UTILITY)" ]; then
+          READY=0 # Specific Entware utility found
+        else
+          # Not all Entware utilities exist as a stand-alone package e.g. 'find' is in package 'findutils'
+          # 	opkg files findutils
+          #
+          # 	Package findutils (4.6.0-1) is installed on root and has the following files:
+          # 	/opt/bin/xargs
+          # 	/opt/bin/find
+          # Add 'executable' as 'stubby' leaves behind two directories containing the string 'stubby'
+          if [ -d /opt ] && [ -n "$(find /opt/ -type f -executable -name $ENTWARE_UTILITY)" ]; then
+            READY=0 # Specific Entware utility found
+          fi
+        fi
+      else
+        READY=0 # Entware utilities ready
       fi
-      sleep 1
-      logger -st "($(basename $0))" $$ "Entware" $ENTWARE_UTILITY "not available - wait time" $((MAX_TRIES - TRIES-1))" secs left"
-      local TRIES=$((TRIES + 1))
-   done
+      break
+    fi
+    sleep 1
+    logger -st "($(basename $0))" $$ "Entware" "$ENTWARE_UTILITY" "not available - wait time" $((MAX_TRIES - TRIES - 1))" secs left"
+    TRIES=$((TRIES + 1))
+  done
 
-   return $READY
+  return $READY
 }
 
 # check if /jffs/configs/dnsmasq.conf.add contains 'ipset=' entry for the domains
-check_dnsmasq () {
+check_dnsmasq() {
 
-    if [ -s /jffs/configs/dnsmasq.conf.add ]; then  # dnsmasq.conf.add file exists
-        if [ "$(grep -c "$1" "/jffs/configs/dnsmasq.conf.add")" -eq "1" ]; then  # see if line exists for $IPSET_NAME
-			if [ "$2" == "del" ];then
-				sed -i "/^ipset.*${IPSET_NAME}$/d" /jffs/configs/dnsmasq.conf.add
-				logger -st "($(basename "$0"))" $$ "'"ipset=$1"'" deleted from "'/jffs/configs/dnsmasq.conf.add'"
-			fi
-		else
-			printf "ipset=$1\n" >> /jffs/configs/dnsmasq.conf.add # add 'ipset=' domains entry to dnsmasq.conf.add
-		fi
-        service restart_dnsmasq > /dev/null 2>&1
+  if [ -s /jffs/configs/dnsmasq.conf.add ]; then # dnsmasq.conf.add file exists
+    if [ "$(grep -c "$1" "/jffs/configs/dnsmasq.conf.add")" -eq "1" ]; then # see if line exists for $IPSET_NAME
+      if [ "$2" = "del" ]; then
+        sed -i "/^ipset.*${IPSET_NAME}$/d" /jffs/configs/dnsmasq.conf.add
+        logger -st "($(basename "$0"))" $$ "'"ipset=$1"'" deleted from "'/jffs/configs/dnsmasq.conf.add'"
+      fi
     else
-		if [ "$2" != "del" ];then
-			printf "ipset=$1\n" > /jffs/configs/dnsmasq.conf.add # dnsmasq.conf.add does not exist, create dnsmasq.conf.add
-			logger -st "($(basename "$0"))" $$ "'"ipset=$1"'" added to "'/jffs/configs/dnsmasq.conf.add'"
-			service restart_dnsmasq > /dev/null 2>&1
-		fi
+      printf "ipset=$1\n" >>/jffs/configs/dnsmasq.conf.add # add 'ipset=' domains entry to dnsmasq.conf.add
     fi
+    service restart_dnsmasq >/dev/null 2>&1
+  else
+    if [ "$2" != "del" ]; then
+      printf "ipset=$1\n" >/jffs/configs/dnsmasq.conf.add # dnsmasq.conf.add does not exist, create dnsmasq.conf.add
+      logger -st "($(basename "$0"))" $$ "'"ipset=$1"'" added to "'/jffs/configs/dnsmasq.conf.add'"
+      service restart_dnsmasq >/dev/null 2>&1
+    fi
+  fi
 }
 
-check_ipset_list () {
-	if [ "$2" != "del" ];then
-		if [ "$(ipset list -n $1 2>/dev/null)" != "$1" ]; then #does ipset list exist?
-			if [ -s "$DIR/$1" ]; then # does $1 ipset restore file exist?
-				ipset restore -! < "$DIR/$1"   # Restore ipset list if restore file exists at $DIR/$1
-				logger -st "($(basename "$0"))" $$ IPSET restored: $1 from "'$DIR/$1'"
-			else
-				ipset create $1 hash:net family inet hashsize 1024 maxelem 65536  # No restore file, so create $1 ipset list from scratch
-				logger -st "($(basename "$0"))" $$ IPSET created: $1 hash:net family inet hashsize 1024 maxelem 65536
-			fi
-		fi
-	else
-		if [ "$(ipset list -n $1 2>/dev/null)" == "$1" ]; then
-			ipset destroy $1
-			logger -st "($(basename "$0"))" $$ IPSET $1 deleted!
-		fi
-	fi
+check_ipset_list() {
+  if [ "$2" != "del" ]; then
+    if [ "$(ipset list -n $1 2>/dev/null)" != "$1" ]; then #does ipset list exist?
+      if [ -s "$DIR/$1" ]; then # does $1 ipset restore file exist?
+        ipset restore -! <"$DIR/$1" # Restore ipset list if restore file exists at $DIR/$1
+        logger -st "($(basename "$0"))" $$ IPSET restored: $1 from "'$DIR/$1'"
+      else
+        ipset create $1 hash:net family inet hashsize 1024 maxelem 65536 # No restore file, so create $1 ipset list from scratch
+        logger -st "($(basename "$0"))" $$ IPSET created: $1 hash:net family inet hashsize 1024 maxelem 65536
+      fi
+    fi
+  else
+    if [ "$(ipset list -n $1 2>/dev/null)" = "$1" ]; then
+      ipset destroy $1
+      logger -st "($(basename "$0"))" $$ IPSET $1 deleted!
+    fi
+  fi
 }
 
 # if IPSET is older than 24 hours, save the current IPSET list to disk
-check_restore_file_age () {
-    if [ -s "$DIR" ]; then
-        if [ "$(find $DIR -name $1 -mtime +1 -print 2&>/dev/null )" = "$DIR/$1" ] ; then
-            ipset save $1 > "$DIR/$1"
-        fi
+check_restore_file_age() {
+  if [ -s "$DIR" ]; then
+    if [ "$(find $DIR -name $1 -mtime +1 -print 2>/dev/null)" = "$DIR/$1" ]; then
+      ipset save $1 >"$DIR/$1"
     fi
+  fi
 }
 
 # If cronjob to back up the DOMAINS ipset list every 24 hours @ 2:00 AM does not exist, then create it
-check_cron_job () {
-    #cru l | grep $1_ipset_list
-	cru l | grep $1	2&>/dev/null						# Martineau Fix
-    if [ "$?" = "1" ]; then  # no cronjob entry found, create it
-		if [ "$2" != "del" ];then
-			cru a $1 "0 2 * * * ipset save $1 > $DIR/$1"
-			logger -st "($(basename "$0"))" $$ CRON schedule created: "#"$1"#" "'0 2 * * * ipset save $1'"
-		fi
-	else
-		if [ "$2" == "del" ];then
-			cru d $1 "0 2 * * * ipset save $1"
-			logger -st "($(basename "$0"))" $$ CRON schedule deleted: "#"$1"#" "'0 2 * * * ipset save $1'"
-		fi
+check_cron_job() {
+  #cru l | grep $1_ipset_list
+  cru l | grep $1 2>/dev/null # Martineau Fix
+  if [ "$?" = "1" ]; then # no cronjob entry found, create it
+    if [ "$2" != "del" ]; then
+      cru a $1 "0 2 * * * ipset save $1 > $DIR/$1"
+      logger -st "($(basename "$0"))" $$ CRON schedule created: "#$1#" "'0 2 * * * ipset save $1'"
     fi
+  else
+    if [ "$2" = "del" ]; then
+      cru d $1 "0 2 * * * ipset save $1"
+      logger -st "($(basename "$0"))" $$ CRON schedule deleted: "#$1#" "'0 2 * * * ipset save $1'"
+    fi
+  fi
 }
 
+Check_Lock "$@"
 #======================================================================================Martineau Hack
 IPSET_NAME=
 DOMAINS_LIST=
 DIR="/opt/tmp"
 
-
-AUTOSCAN=															# v1.3
-if [ "$(echo "$@" | grep -c 'autoscan')" -gt 0 ];then
-	AUTOSCAN=$(echo "$@" | sed -n "s/^.*autoscan=//p" | awk '{print $1}')
-	[ -z "$AUTOSCAN" ] && AUTOSCAN="/opt/var/log/dnsmasq.log"
-	if [ -n "$AUTOSCAN" ];then
-		if [ ! -f "$AUTOSCAN" ];then
-			logger -st "($(basename "$0"))" $$ "ERROR 'autoscan=$AUTOSCAN' file NOT found!"
-			exit 99
-		fi
-	fi
+AUTOSCAN= # v1.3
+if [ "$(echo "$@" | grep -c 'autoscan')" -gt 0 ]; then
+  AUTOSCAN=$(echo "$@" | sed -n "s/^.*autoscan=//p" | awk '{print $1}')
+  [ -z "$AUTOSCAN" ] && AUTOSCAN="/opt/var/log/dnsmasq.log"
+  if [ -n "$AUTOSCAN" ]; then
+    if [ ! -f "$AUTOSCAN" ]; then
+      logger -st "($(basename "$0"))" $$ "ERROR 'autoscan=$AUTOSCAN' file NOT found!"
+      exit 99
+    fi
+  fi
 fi
 
 #if [ ! -z "$1" ];then
@@ -199,52 +196,50 @@ fi
 #else
 #	logger -st "($(basename "$0"))" $$ Warning missing arg1 "'destination_target' 0-WAN or 1-5=VPN," WAN assumed!
 #fi
-if [ -n "$1" ];then
-	IPSET_NAME=$1
+if [ -n "$1" ]; then
+  IPSET_NAME=$1
 else
-	logger -st "($(basename "$0"))" $$ ERROR missing arg2 "'ipset_name'"
-	exit 97
+  logger -st "($(basename "$0"))" $$ ERROR missing arg2 "'ipset_name'"
+  exit 97
 fi
-if [ -n "$2" ] && [ -z "$AUTOSCAN" ];then								# v1.3
-	DOMAINS_LIST="$2"
+if [ -n "$2" ] && [ -z "$AUTOSCAN" ]; then # v1.3
+  DOMAINS_LIST="$2"
 else
-	if [ -z "$AUTOSCAN" ];then								# v1.3
-		logger -st "($(basename "$0"))" $$ ERROR missing arg3 "'domain_list'"
-		exit 98
-	else
-		DOMAIN=$2
-		# So having extracted the matching domains						# v1.3
-		# Extract only the two-part TL domain i.e. disregard the sub-domains
-		DOMAINS_LIST=$(grep $DOMAIN $AUTOSCAN | grep reply | awk '{print $(NF-2)}' | awk -F\. '{print $(NF-1) FS $NF}' | sort | uniq | tr '\n' ',')
-	fi
+  if [ -z "$AUTOSCAN" ]; then # v1.3
+    logger -st "($(basename "$0"))" $$ ERROR missing arg3 "'domain_list'"
+    exit 98
+  else
+    DOMAIN=$2
+    # So having extracted the matching domains						# v1.3
+    # Extract only the two-part TL domain i.e. disregard the sub-domains
+    DOMAINS_LIST=$(grep $DOMAIN $AUTOSCAN | grep reply | awk '{print $(NF-2)}' | awk -F\. '{print $(NF-1) FS $NF}' | sort | uniq | tr '\n' ',')
+  fi
 fi
 
-if [ "$(echo "$@" | grep -c 'dir=')" -gt 0 ];then
-	DIR=$(echo "$@" | sed -n "s/^.*dir=//p" | awk '{print $1}')	# v1.2 Mount point/directory for backups
+if [ "$(echo "$@" | grep -c 'dir=')" -gt 0 ]; then
+  DIR=$(echo "$@" | sed -n "s/^.*dir=//p" | awk '{print $1}') # v1.2 Mount point/directory for backups
 fi
 
-if [ -z "$IPSET_NAME" ] || [ -z "$DOMAINS_LIST" ];then
-	logger -st "($(basename "$0"))" $$ ERROR missing args "'target destination' 'ipset_name' 'domain_list'"
-	exit 98
+if [ -z "$IPSET_NAME" ] || [ -z "$DOMAINS_LIST" ]; then
+  logger -st "($(basename "$0"))" $$ ERROR missing args "'target destination' 'ipset_name' 'domain_list'"
+  exit 98
 fi
 
-DOMAINS_LIST=$(echo "$DOMAINS_LIST" | sed 's/,$//' | tr ',' '/')	# v1.3
-DNSMASQ_ENTRY="/"${DOMAINS_LIST}"/"${IPSET_NAME}
+DOMAINS_LIST=$(echo "$DOMAINS_LIST" | sed 's/,$//' | tr ',' '/') # v1.3
+DNSMASQ_ENTRY="/$DOMAINS_LIST/$IPSET_NAME"
 
 # Delete mode?
-if [ "$(echo $@ | grep -cw 'del')" -gt 0 ];then
-	check_dnsmasq			"$DNSMASQ_ENTRY"	"del"
-	check_cron_job			"$IPSET_NAME"		"del"
-	check_ipset_list		"$IPSET_NAME"		"del"
+if [ "$(echo "$@" | grep -cw 'del')" -gt 0 ]; then
+  check_dnsmasq "$DNSMASQ_ENTRY" "del"
+  check_cron_job "$IPSET_NAME" "del"
+  check_ipset_list "$IPSET_NAME" "del"
 else
-#==================================================================================================
-	Chk_Entware
-	check_dnsmasq 			"$DNSMASQ_ENTRY"			# Martineau Hack
-	check_ipset_list		"$IPSET_NAME"				# Martineau Hack
-	check_restore_file_age	"$IPSET_NAME"				# Martineau Hack
-	check_cron_job			"$IPSET_NAME"				# Martineau Hack
+  #==================================================================================================
+  Chk_Entware
+  check_dnsmasq "$DNSMASQ_ENTRY"       # Martineau Hack
+  check_ipset_list "$IPSET_NAME"       # Martineau Hack
+  check_restore_file_age "$IPSET_NAME" # Martineau Hack
+  check_cron_job "$IPSET_NAME"         # Martineau Hack
+  if [ "$lock_load_DNSMASQ_ipset" = "true" ]; then rm -rf "/tmp/load_DNSMASQ_ipset.lock"; fi
 fi
 logger -st "($(basename "$0"))" $$ Completed Script Execution
-
-}
-main "$@"											# Martineau Hack

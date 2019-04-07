817055c0264b0c8319084ced3447b6571b8c3e3e
Xentrk <xentrk@x3mtek.com>
2019-04-07 12:49:06 +0700

    updated install_x3mRouting.sh for vers update check & shell scripts

--- install_x3mRouting.sh
+++ install_x3mRouting.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 ####################################################################################################
 # Script: install_x3mRouting.sh
-# Original Author: Xentrk
-# Last Updated Date: 31-March-2019
+# Author: Xentrk
+# Last Updated Date: 7-April-2019
 #
 # Description:
-#  Install, Update or Remove the Asuswrt-Merlin-Selective-Routing repository
+#  Install, Update or Remove the x3mRouting repository
 #
 # Acknowledgement:
 #  This project would never have been made possible if not for @Martineau on snbfourms.com
@@ -15,7 +15,7 @@
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin$PATH
 logger -t "($(basename "$0"))" "$$ Starting Script Execution ($(if [ -n "$1" ]; then echo "$1"; else echo "menu"; fi))"
 VERSION="1.0.0"
-GIT_REPO="Asuswrt-Merlin-Selective-Routing"
+GIT_REPO="x3mRouting"
 GITHUB_DIR="https://raw.githubusercontent.com/Xentrk/$GIT_REPO/master"
 
 # Uncomment the line below for debugging
@@ -26,6 +26,7 @@ COLOR_WHITE='\033[0m'
 COLOR_GREEN='\e[0;32m'
 
 welcome_message() {
+  clear
   while true; do
     printf '\n_______________________________________________________________________\n'
     printf '|                                                                     |\n'
@@ -42,39 +43,53 @@ welcome_message() {
     printf '| Requirements: jffs partition and USB drive with entware installed   |\n'
     printf '|                                                                     |\n'
     printf '| See the project repository at                                       |\n'
-    printf '| %bhttps://github.com/Xentrk/Asuswrt-Merlin-Selective-Routing%b           |\n' "$COLOR_GREEN" "$COLOR_WHITE"
+    printf '| %bhttps://github.com/Xentrk/x3mRouting%b                         |\n' "$COLOR_GREEN" "$COLOR_WHITE"
     printf '| for helpful tips.                                                   |\n'
     printf '|_____________________________________________________________________|\n\n'
-    if [ "$1" = "uninstall" ]; then
-      menu1="2"
+    localmd5="$(md5sum "$0" | awk '{print $1}')"
+    remotemd5="$(curl -fsL --retry 3 "${GITHUB_DIR}/install_x3mRouting.sh" | md5sum | awk '{print $1}')"
+    if [ "$localmd5" != "$remotemd5" ]; then
+      update_installer
     else
-      localmd5="$(md5sum "$0" | awk '{print $1}')"
-      remotemd5="$(curl -fsL --retry 3 "${GITHUB_DIR}/install_x3mRouting.sh" | md5sum | awk '{print $1}')"
-      if [ "$localmd5" != "$remotemd5" ]; then
-        update_installer
-      fi
-      printf '%b[1]%b = Install Selective Routing for LAN Clients\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
-      printf '%b[2]%b = Install Selective Routing for IPSET lists\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
-      printf '%b[3]%b = Update current Selective Routing installation\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
-      printf '%b[4]%b = Remove Selective Routing Repository\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
+      Main_Menu
+    fi
+  done
+}
+
+Main_Menu() {
+      printf '%b[1]%b = Install x3mRouting for LAN Clients Shell Scripts\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
+      printf '%b[2]%b = Install x3mRouting OpenVPN Client GUI & IPSET Shell Scripts\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
+      printf '%b[3]%b = Install x3mRouting IPSET Shell Scripts\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
+      printf '%b[4]%b = Check for updates to existing x3mRouting installation\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
+      printf '%b[5]%b = Force update existing x3mRouting installation\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
+      printf '%b[6]%b = Remove x3mRouting Repository\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
       printf '\n%b[e]%b = Exit Script\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
       printf '\n%bOption ==>%b ' "${COLOR_GREEN}" "${COLOR_WHITE}"
       read -r "menu1"
-    fi
+
     case "$menu1" in
     1)
       install_x3mRouting_LAN_clients
       break
       ;;
     2)
-      install_x3mRouting_IPSET_lists
+      install_x3mRouting_GUI
       break
       ;;
     3)
-      update_current_installation
+      install_x3mRouting_shell_scripts
       break
       ;;
+
     4)
+      confirm_update
+      break
+      ;;
+    5)
+      confirm_update force
+      break
+      ;;
+    6)
       validate_removal
       break
       ;;
@@ -86,19 +101,18 @@ welcome_message() {
       printf '%bInvalid Option%b %s%b Please enter a valid option\n' "$COLOR_RED" "$COLOR_GREEN" "$menu1" "$COLOR_WHITE"
       ;;
     esac
-  done
 }
 
 validate_removal() {
   while true; do
-    printf 'Are you sure you want to uninstall the %bAsuswrt-Merlin-Selective-Routing Repository%b\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
+    printf 'Are you sure you want to uninstall the %bx3mRouting%b repository\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
     printf 'and all changes made by the installer?\n'
     printf '%b[y]%b --> Yes \n' "${COLOR_GREEN}" "${COLOR_WHITE}"
     printf '%b[n]%b --> Cancel\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
     printf '%b[e]%b --> Exit Script\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
     printf '\n%bOption ==>%b ' "${COLOR_GREEN}" "${COLOR_WHITE}"
-    read -r "menu3"
-    case "$menu3" in
+    read -r "menu_validate_removal"
+    case "$menu_validate_removal" in
     y)
       remove_existing_installation
       break
@@ -118,22 +132,27 @@ validate_removal() {
   done
 }
 
-update_current_installation() {
+confirm_update() {
+
   while true; do
     printf '\n\nThis option will check your current installation and update any files that have changed\n'
     printf 'since you last installed the repository.  Updating is highly recommended to get the most recent.\n'
     printf 'files. Chosing this option will not update missing files. Select the install option from the\n'
-    printf 'menu to reinstall the files\n'
+    printf 'menu to reinstall missing files\n\n'
     printf 'Would you like to download and update any new version now?\n'
     echo "[1]  --> Yes"
     echo "[2]  --> No"
     echo
     printf "[1-2]: "
-    read -r "menu_update_current_installation"
+    read -r "confirm_update_option"
     echo
-    case "$menu_update_current_installation" in
+    case "$confirm_update_option" in
     1)
-      update_current_installation_2
+      if [ -z "$1" ]; then
+        Update_Version
+      else
+        Update_Version force
+      fi
       welcome_message
       break
       ;;
@@ -142,14 +161,73 @@ update_current_installation() {
       break
       ;;
     *)
-      echo "[*] $menu_update_installer Isn't An Option!"
+      echo "[*] $confirm_update_option Isn't An Option!"
       ;;
     esac
   done
 }
 
-update_current_installation_2() {
-  DIR="/jffs/scripts/Asuswrt-Merlin-Selective-Routing"
+### Code for update code functions inspired by https://github.com/Adamm00 - credit to @Adamm
+### and https://github.com/jackyaz/spdMerlin - credit to Jack Yaz
+Update_Version(){
+  DIR="/jffs/scripts/x3mRouting"
+  if [ -d "$DIR" ]; then
+    for FILE in x3mRouting_client_nvram.sh \
+                x3mRouting_config.sh \
+                vpnrouting.sh \
+                updown.sh \
+                Advanced_OpenVPNClient_Content.asp \
+                load_MANUAL_ipset.sh \
+                load_ASN_ipset.sh \
+                load_DNSMASQ_ipset.sh \
+                load_AMAZON_ipset.sh; \
+                load_MANUAL_ipset_iface.sh \
+                load_ASN_ipset_iface.sh \
+                load_DNSMASQ_ipset_iface.sh \
+                load_AMAZON_ipset_iface.sh; do
+if [ -s "$DIR/$FILE" ]; then
+	if [ -z "$1" ]; then
+		do_force_update="false"
+		localver=$(grep "VERSION=" "$DIR/$FILE" | grep -m1 -oE 'v[0-9]{1,2}([.][0-9]{1,2})([.][0-9]{1,2})')
+	#	/usr/sbin/curl -fsL --retry 3 "$SPD_REPO/$SPD_NAME_LOWER.sh" | grep -qF "jackyaz" || { Print_Output "true" "404 error detected - stopping update" "$ERR"; return 1; }
+		serverver=$(/usr/sbin/curl -fsL --retry 3 "$GITHUB_DIR/$FILE" | grep "VERSION=" | grep -m1 -oE 'v[0-9]{1,2}([.][0-9]{1,2})([.][0-9]{1,2})')
+		if [ "$localver" != "$serverver" ]; then
+      printf 'New version of $FILE available - updating to $serverver\n'
+			downloaded_file "$DIR" "$FILE"
+      chmod 0755 "$DIR/$FILE"
+		else
+      localmd5="$(md5sum "$DIR/$FILE" | awk '{print $1}')"
+      remotemd5="$(curl -fsL --retry 3 "$GITHUB_DIR/$FILE" | md5sum | awk '{print $1}')"
+      if [ "$localmd5" != "$remotemd5" ]; then
+        "MD5 hash of $FILE does not match - downloading updated $serverver"
+				downloaded_file "$DIR" "$FILE"
+        chmod 0755 "$DIR/$FILE"
+      else
+        printf 'No new version - latest is $localver\n'
+			fi
+		fi
+	fi
+
+	case "$1" in
+		force)
+			serverver=$(/usr/sbin/curl -fsL --retry 3 "$GITHUB_DIR/$FILE" | grep "VERSION=" | grep -m1 -oE 'v[0-9]{1,2}([.][0-9]{1,2})([.][0-9]{1,2})')
+			printf 'Downloading latest version ($serverver) of $FILE\n'
+      downloaded_file "$DIR" "$FILE"
+			exit 0
+		;;
+	esac
+done
+
+else
+  echo "Project Repository directory $DIR not found"
+  echo "Select the install option from the main menu to install the respository"
+fi
+
+}
+############################################################################
+
+update_current_installation() {
+  DIR="/jffs/scripts/x3mRouting"
   if [ -d "$DIR" ]; then
     for FILE in x3mRouting_client_nvram.sh \
                 x3mRouting_config.sh \
@@ -174,7 +252,7 @@ update_current_installation_2() {
 }
 
 remove_existing_installation() {
-  echo "Starting removal of Asuswrt-Merlin-Selective-Routing Repository"
+  echo "Starting removal of x3mRouting Repository"
 
   # Remove the jq package
   Chk_Entware jq
@@ -187,7 +265,7 @@ remove_existing_installation() {
 
   # Remove entries from /jffs/scripts/init-start
   if [ -s "/jffs/scripts/init-start" ]; then # file exists
-    for PARM in "sh /jffs/scripts/Asuswrt-Merlin-Selective-Routing/mount_files_lan.sh" "sh /jffs/scripts/Asuswrt-Merlin-Selective-Routing/mount_files_ipset.sh"; do
+    for PARM in "sh /jffs/scripts/x3mRouting/mount_files_lan.sh" "sh /jffs/scripts/x3mRouting/mount_files_gui.sh"; do
       if grep -q "$PARM" "/jffs/scripts/init-start"; then # see if line exists
         sed -i "\\~$PARM~d" "/jffs/scripts/init-start"
         echo "$PARM entry removed from /jffs/scripts/init-start"
@@ -197,8 +275,8 @@ remove_existing_installation() {
   fi
   # TBD - ckeck if only the she-bang exists and del file it it does
 
-  # Purge /jffs/scripts/Asuswrt-Merlin-Selective-Routing directory
-  for DIR in "/jffs/scripts/Asuswrt-Merlin-Selective-Routing"; do
+  # Purge /jffs/scripts/x3mRouting directory
+  for DIR in "/jffs/scripts/x3mRouting"; do
     if [ -d "$DIR" ]; then
       if ! rm "$DIR"/* >/dev/null 2>&1; then
         printf '\nNo files found to remove in %b%s%b\n' "$COLOR_GREEN" "$DIR" "$COLOR_WHITE"
@@ -258,7 +336,7 @@ Chk_Entware() {
 }
 
 create_project_directory() {
-  for DIR in "/jffs/scripts/Asuswrt-Merlin-Selective-Routing"; do
+  for DIR in "/jffs/scripts/x3mRouting"; do
     if [ ! -d "$DIR" ]; then
       if mkdir -p "$DIR" >/dev/null 2>&1; then
         printf "Created project directory %b%s%b\\n" "${COLOR_GREEN}" "${DIR}" "${COLOR_WHITE}"
@@ -276,6 +354,9 @@ download_file() {
   STATUS="$(curl --retry 3 -sL -w '%{http_code}' "$GITHUB_DIR/$FILE" -o "$DIR/$FILE")"
   if [ "$STATUS" -eq "200" ]; then
     printf '%b%s%b downloaded successfully\n' "$COLOR_GREEN" "$FILE" "$COLOR_WHITE"
+    if [ "$(echo "$FILE" | grep -cw '.sh')" -gt 0 ]; then
+      chmod 755 "$DIR/$FILE"
+    fi
   else
     printf '%b%s%b download failed with curl error %s\n' "$COLOR_GREEN" "$FILE" "$COLOR_WHITE" "$STATUS"
     printf 'Rerun the install %bx3mRouting for LAN Clients%b option\n' "$COLOR_GREEN" "$COLOR_WHITE" "$COLOR_GREEN" "$COLOR_WHITE"
@@ -292,16 +373,15 @@ exit_message() {
   printf '    \ \/ / |_  | %b %b \  __|/ _ \| |/ /   /  _//    \| %b %b \ \n' "\`" "\`" "\`" "\`"
   printf '     /  /  __| | | | |  |_ | __/|   <   (  (_ | [] || | | |  \n'
   printf '    /_/\_\|___ |_|_|_|\___|\___||_|\_\[] \___\\\____/|_|_|_| \n\n\n'
-  rm -rf /tmp/stubby.lock
   exit 0
 }
 
 init_start_update() {
   PARM=$1
   # checking for condition that user previously installed LAN clients. if so, remove mount_files_lan.sh entry
-  if [ -s "/jffs/scripts/init-start" ] && [ "$PARM" = "sh /jffs/scripts/Asuswrt-Merlin-Selective-Routing/mount_files_ipset.sh" ]; then
-    if grep -q "sh /jffs/scripts/Asuswrt-Merlin-Selective-Routing/mount_files_lan.sh" "/jffs/scripts/init-start"; then
-      sed -i "\\~/jffs/scripts/Asuswrt-Merlin-Selective-Routing/mount_files_lan.sh~d" "/jffs/scripts/init-start"
+  if [ -s "/jffs/scripts/init-start" ] && [ "$PARM" = "sh /jffs/scripts/x3mRouting/mount_files_gui.sh" ]; then
+    if grep -q "sh /jffs/scripts/x3mRouting/mount_files_lan.sh" "/jffs/scripts/init-start"; then
+      sed -i "\\~/jffs/scripts/x3mRouting/mount_files_lan.sh~d" "/jffs/scripts/init-start"
     fi
   fi
 
@@ -322,51 +402,64 @@ init_start_update() {
 }
 
 install_x3mRouting_LAN_clients() {
-  # check for stubby folders with no files
   create_project_directory
-  download_file /jffs/scripts/Asuswrt-Merlin-Selective-Routing x3mRouting_client_nvram.sh
-  download_file /jffs/scripts/Asuswrt-Merlin-Selective-Routing x3mRouting_config.sh
-  download_file /jffs/scripts/Asuswrt-Merlin-Selective-Routing vpnrouting.sh
-  download_file /jffs/scripts/Asuswrt-Merlin-Selective-Routing updown.sh
-  download_file /jffs/scripts/Asuswrt-Merlin-Selective-Routing mount_files_lan.sh
-  init_start_update "sh /jffs/scripts/Asuswrt-Merlin-Selective-Routing/mount_files_lan.sh"
+  download_file /jffs/scripts/x3mRouting x3mRouting_client_nvram.sh
+  download_file /jffs/scripts/x3mRouting x3mRouting_config.sh
+  download_file /jffs/scripts/x3mRouting vpnrouting.sh
+  download_file /jffs/scripts/x3mRouting updown.sh
+  download_file /jffs/scripts/x3mRouting mount_files_lan.sh
+  init_start_update "sh /jffs/scripts/x3mRouting/mount_files_lan.sh"
   sh /jffs/scripts/init-start
   echo "Installation of x3mRouting for LAN Clients completed"
 }
 
-install_x3mRouting_IPSET_lists() {
-  if Chk_Entware; then
-    if opkg update >/dev/null 2>&1; then
-      echo "Entware package list successfully updated"
-      # load_AMAZON_ipset.sh requires the jq package to extract the Amazon json file
-      if opkg install jq --force-downgrade; then
-        echo "jq successfully installed"
-      else
-        echo "An error occurred installing jq"
-        exit 1
-      fi
+Check_Requirements () {
+
+if Chk_Entware; then
+  if opkg update >/dev/null 2>&1; then
+    echo "Entware package list successfully updated"
+    # load_AMAZON_ipset.sh requires the jq package to extract the Amazon json file
+    if opkg install jq --force-downgrade; then
+      echo "jq successfully installed"
     else
-      echo "An error occurred updating Entware package list"
+      echo "An error occurred installing jq"
       exit 1
     fi
   else
-    echo "You must first install Entware before proceeding"
-    printf 'Exiting %s\n' "$(basename "$0")"
+    echo "An error occurred updating Entware package list"
     exit 1
   fi
+else
+  echo "You must first install Entware before proceeding"
+  printf 'Exiting %s\n' "$(basename "$0")"
+  exit 1
+fi
+}
 
+install_x3mRouting_GUI() {
+  Check_Requirements
   create_project_directory
-  download_file /jffs/scripts/Asuswrt-Merlin-Selective-Routing vpnrouting.sh
-  download_file /jffs/scripts/Asuswrt-Merlin-Selective-Routing updown.sh
-  download_file /jffs/scripts/Asuswrt-Merlin-Selective-Routing Advanced_OpenVPNClient_Content.asp
-  download_file /jffs/scripts/Asuswrt-Merlin-Selective-Routing load_MANUAL_ipset.sh
-  download_file /jffs/scripts/Asuswrt-Merlin-Selective-Routing load_ASN_ipset.sh
-  download_file /jffs/scripts/Asuswrt-Merlin-Selective-Routing load_DNSMASQ_ipset.sh
-  download_file /jffs/scripts/Asuswrt-Merlin-Selective-Routing load_AMAZON_ipset.sh
-  download_file /jffs/scripts/Asuswrt-Merlin-Selective-Routing mount_files_ipset.sh
-  init_start_update "sh /jffs/scripts/Asuswrt-Merlin-Selective-Routing/mount_files_ipset.sh"
+  download_file /jffs/scripts/x3mRouting vpnrouting.sh
+  download_file /jffs/scripts/x3mRouting updown.sh
+  download_file /jffs/scripts/x3mRouting Advanced_OpenVPNClient_Content.asp
+  download_file /jffs/scripts/x3mRouting load_MANUAL_ipset.sh
+  download_file /jffs/scripts/x3mRouting load_ASN_ipset.sh
+  download_file /jffs/scripts/x3mRouting load_DNSMASQ_ipset.sh
+  download_file /jffs/scripts/x3mRouting load_AMAZON_ipset.sh
+  download_file /jffs/scripts/x3mRouting mount_files_gui.sh
+  init_start_update "sh /jffs/scripts/x3mRouting/mount_files_gui.sh"
   sh /jffs/scripts/init-start
   echo "Installation of x3mRouting for IPSET lists completed"
+}
+
+install_x3mRouting_shell_scripts() {
+  Check_Requirements
+  create_project_directory
+  download_file /jffs/scripts/x3mRouting load_MANUAL_ipset_iface.sh
+  download_file /jffs/scripts/x3mRouting load_ASN_ipset_iface.sh
+  download_file /jffs/scripts/x3mRouting load_DNSMASQ_ipset_iface.sh
+  download_file /jffs/scripts/x3mRouting load_AMAZON_ipset_iface.sh
+  echo "Installation of x3mRouting for IPSET Shell Scripts completed"
 }
 
 update_installer() {
@@ -388,7 +481,7 @@ update_installer() {
       break
       ;;
     2)
-      welcome_message
+      Main_Menu
       break
       ;;
     *)
@@ -398,5 +491,4 @@ update_installer() {
   done
 }
 
-clear
 welcome_message

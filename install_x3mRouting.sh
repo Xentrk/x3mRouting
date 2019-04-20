4a56b1c11e014e24c06ff1856ef4dccf87eabe32
Xentrk <xentrk@x3mtek.com>
2019-04-20 12:13:56 +0700

    finished testing install_x3mRouting.sh

--- install_x3mRouting.sh
+++ install_x3mRouting.sh
@@ -2,8 +2,7 @@
 ####################################################################################################
 # Script: install_x3mRouting.sh
 # Author: Xentrk
-# Last Updated Date: 7-April-2019
-# Version 0.0.1
+# Last Updated Date: 20-April-2019
 #
 # Description:
 #  Install, Update or Remove the x3mRouting repository
@@ -12,8 +11,9 @@
 #  This project would never have been made possible if not for @Martineau on snbfourms.com
 #  providing his Selective Routing knowledge and expertise. I am extemely GRATEFUL!
 #
-# Code for update code functions inspired by https://github.com/Adamm00 - credit to @Adamm
-# and https://github.com/jackyaz/spdMerlin - credit to Jack Yaz
+# Code for update code functions inspired by
+#    https://github.com/Adamm00 - credit to @Adamm
+#    https://github.com/jackyaz/spdMerlin - credit to Jack Yaz
 ####################################################################################################
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin$PATH
 logger -t "($(basename "$0"))" "$$ Starting Script Execution ($(if [ -n "$1" ]; then echo "$1"; else echo "menu"; fi))"
@@ -23,7 +23,7 @@ GITHUB_DIR="https://raw.githubusercontent.com/Xentrk/$GIT_REPO/master"
 LOCAL_REPO="/jffs/scripts/x3mRouting"
 
 # Uncomment the line below for debugging
-#set -x
+set -x
 
 COLOR_RED='\033[0;31m'
 COLOR_WHITE='\033[0m'
@@ -34,7 +34,7 @@ welcome_message() {
   while true; do
     printf '\n_______________________________________________________________________\n'
     printf '|                                                                     |\n'
-    printf '|  Welcome to the %bx3mRouting%b installation script |\n' "$COLOR_GREEN" "$COLOR_WHITE"
+    printf '|  Welcome to the %bx3mRouting%b installation script                      |\n' "$COLOR_GREEN" "$COLOR_WHITE"
     printf '|  Version %s by Xentrk                                            |\n' "$VERSION"
     printf '|         ____        _         _                                     |\n'
     printf '|        |__  |      | |       | |                                    |\n'
@@ -46,8 +46,7 @@ welcome_message() {
     printf '|                                                                     |\n'
     printf '| Requirements: jffs partition and USB drive with entware installed   |\n'
     printf '|                                                                     |\n'
-    printf '| See the project repository at                                       |\n'
-    printf '| %bhttps://github.com/Xentrk/x3mRouting%b                         |\n' "$COLOR_GREEN" "$COLOR_WHITE"
+    printf '| See the project repository at %bhttps://github.com/Xentrk/x3mRouting%b  |\n' "$COLOR_GREEN" "$COLOR_WHITE"
     printf '| for helpful tips.                                                   |\n'
     printf '|_____________________________________________________________________|\n\n'
     localmd5="$(md5sum "$0" | awk '{print $1}')"
@@ -61,14 +60,14 @@ welcome_message() {
 }
 
 Main_Menu() {
-      printf '%b[1]%b = Install x3mRouting for LAN Clients Shell Scripts\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
-      printf '%b[2]%b = Install x3mRouting OpenVPN Client GUI & IPSET Shell Scripts\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
-      printf '%b[3]%b = Install x3mRouting IPSET Shell Scripts\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
-      printf '%b[4]%b = Check for updates to existing x3mRouting installation\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
-      printf '%b[5]%b = Force update existing x3mRouting installation\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
-      printf '%b[6]%b = Remove x3mRouting Repository\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
-      printf '\n%b[e]%b = Exit Script\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
-      printf '\n%bOption ==>%b ' "${COLOR_GREEN}" "${COLOR_WHITE}"
+      printf '[1] = Install x3mRouting for LAN Clients Shell Scripts\n'
+      printf '[2] = Install x3mRouting OpenVPN Client GUI & IPSET Shell Scripts\n'
+      printf '[3] = Install x3mRouting IPSET Shell Scripts\n'
+      printf '[4] = Check for updates to existing x3mRouting installation\n'
+      printf '[5] = Force update existing x3mRouting installation\n'
+      printf '[6] = Remove x3mRouting Repository\n'
+      printf '\n[e] = Exit Script\n'
+      printf '\nOption ==> '
       read -r "menu1"
 
     case "$menu1" in
@@ -111,9 +110,9 @@ validate_removal() {
   while true; do
     printf 'Are you sure you want to uninstall the %bx3mRouting%b repository\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
     printf 'and all changes made by the installer?\n'
-    printf '%b[y]%b --> Yes \n' "${COLOR_GREEN}" "${COLOR_WHITE}"
-    printf '%b[n]%b --> Cancel\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
-    printf '%b[e]%b --> Exit Script\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
+    printf '[%by%b] --> Yes \n' "${COLOR_GREEN}" "${COLOR_WHITE}"
+    printf '[%bn%b] --> Cancel\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
+    printf '[%be%b] --> Exit Script\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
     printf '\n%bOption ==>%b ' "${COLOR_GREEN}" "${COLOR_WHITE}"
     read -r "menu_validate_removal"
     case "$menu_validate_removal" in
@@ -144,14 +143,14 @@ confirm_update() {
     printf 'files. Chosing this option will not update missing files. Select the install option from the\n'
     printf 'menu to reinstall missing files\n\n'
     printf 'Would you like to check and download any files that have been updated?\n'
-    echo "[1]  --> Yes"
-    echo "[2]  --> No"
+    printf '[%by%b]  --> Yes\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
+    printf '[%bn%b]  --> No\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
     echo
-    printf "[1-2]: "
+    printf '\n%bOption ==>%b ' "${COLOR_GREEN}" "${COLOR_WHITE}"
     read -r "confirm_update_option"
     echo
     case "$confirm_update_option" in
-    1)
+    y)
       if [ -z "$1" ]; then
         Update_Version
       else
@@ -160,7 +159,7 @@ confirm_update() {
       welcome_message
       break
       ;;
-    2)
+    n)
       welcome_message
       break
       ;;
@@ -177,7 +176,7 @@ Update_Version(){
   DIR="$LOCAL_REPO"
   if [ -d "$DIR" ]; then
     for FILE in x3mRouting_client_nvram.sh \
-                x3mRouting_config.sh \
+                x3mRouting_client_config.sh \
                 vpnrouting.sh \
                 updown.sh \
                 Advanced_OpenVPNClient_Content.asp \
@@ -186,76 +185,49 @@ Update_Version(){
                 load_MANUAL_ipset.sh \
                 load_ASN_ipset.sh \
                 load_DNSMASQ_ipset.sh \
-                load_AMAZON_ipset.sh; \
+                load_AMAZON_ipset.sh \
                 load_MANUAL_ipset_iface.sh \
                 load_ASN_ipset_iface.sh \
                 load_DNSMASQ_ipset_iface.sh \
-                load_AMAZON_ipset_iface.sh; do
-if [ -s "$DIR/$FILE" ]; then
-	if [ -z "$1" ]; then
-		do_force_update="false"
-		localver=$(grep "VERSION=" "$DIR/$FILE" | grep -m1 -oE 'v[0-9]{1,2}([.][0-9]{1,2})([.][0-9]{1,2})')
+                load_AMAZON_ipset_iface.sh
+	  do
+	  if [ -s "$DIR/$FILE" ]; then
+		  if [ -z "$1" ]; then
+			  do_force_update="false"
+			  localver=$(grep "VERSION=" "$DIR/$FILE" | grep -m1 -oE '[0-9]{1,2}([.][0-9]{1,2})([.][0-9]{1,2})')
 	#	/usr/sbin/curl -fsL --retry 3 "$SPD_REPO/$SPD_NAME_LOWER.sh" | grep -qF "jackyaz" || { Print_Output "true" "404 error detected - stopping update" "$ERR"; return 1; }
-		serverver=$(/usr/sbin/curl -fsL --retry 3 "$GITHUB_DIR/$FILE" | grep "VERSION=" | grep -m1 -oE 'v[0-9]{1,2}([.][0-9]{1,2})([.][0-9]{1,2})')
-		if [ "$localver" != "$serverver" ]; then
-      printf 'New version of $FILE available - updating to $serverver\n'
-			download_file "$DIR" "$FILE"
-      chmod 0755 "$DIR/$FILE"
-		else
-      localmd5="$(md5sum "$DIR/$FILE" | awk '{print $1}')"
-      remotemd5="$(curl -fsL --retry 3 "$GITHUB_DIR/$FILE" | md5sum | awk '{print $1}')"
-      if [ "$localmd5" != "$remotemd5" ]; then
-        printf 'MD5 hash of $FILE does not match - downloading updated $serverver\n'
-				download_file "$DIR" "$FILE"
-        chmod 0755 "$DIR/$FILE"
-      else
-        printf 'No new version to update - latest is $localver\n'
-			fi
-		fi
-	fi
-
-	case "$1" in
-		force)
-			serverver=$(/usr/sbin/curl -fsL --retry 3 "$GITHUB_DIR/$FILE" | grep "VERSION=" | grep -m1 -oE 'v[0-9]{1,2}([.][0-9]{1,2})([.][0-9]{1,2})')
-			printf 'Downloading latest version ($serverver) of $FILE\n'
-      downloaded_file "$DIR" "$FILE"
-			exit 0
-		;;
-	esac
-done
-
-else
-  echo "Project Repository directory $DIR not found"
-  echo "Select the install option from the main menu to install the respository"
-fi
-
-}
-############################################################################
-
-### Remove this function when testing is completed!
-update_current_installation() {
-  DIR="$LOCAL_REPO"
-  if [ -d "$DIR" ]; then
-    for FILE in x3mRouting_client_nvram.sh \
-                x3mRouting_config.sh \
-                vpnrouting.sh \
-                updown.sh \
-                Advanced_OpenVPNClient_Content.asp \
-                load_MANUAL_ipset.sh \
-                load_ASN_ipset.sh \
-                load_DNSMASQ_ipset.sh \
-                load_AMAZON_ipset.sh; do
-      if [ -s "$DIR/$FILE" ]; then
-        localmd5="$(md5sum "$DIR/$FILE" | awk '{print $1}')"
-        remotemd5="$(curl -fsL --retry 3 "${GITHUB_DIR}/$FILE" | md5sum | awk '{print $1}')"
-        if [ "$localmd5" != "$remotemd5" ]; then
-          downloaded_file "$DIR" "$FILE"
+			  serverver=$(/usr/sbin/curl -fsL --retry 3 "$GITHUB_DIR/$FILE" | grep "VERSION=" | grep -m1 -oE 'v[0-9]{1,2}([.][0-9]{1,2})([.][0-9]{1,2})')
+			  if [ "$localver" != "$serverver" ]; then
+  				echo "New version of $FILE available - updating to $serverver"
+				  download_file "$DIR" "$FILE"
+			  else
+				  localmd5="$(md5sum "$DIR/$FILE" | awk '{print $1}')"
+				  remotemd5="$(curl -fsL --retry 3 "$GITHUB_DIR/$FILE" | md5sum | awk '{print $1}')"
+			    if [ "$localmd5" != "$remotemd5" ]; then
+				    echo "MD5 hash of $FILE does not match - downloading updated $serverver"
+				    download_file "$DIR" "$FILE"
+			    else
+				    echo "No new version of $FILE to update"
+			    fi
         fi
-    done
+      fi
+
+
+		case "$1" in
+			force)
+				serverver=$(/usr/sbin/curl -fsL --retry 3 "$GITHUB_DIR/$FILE" | grep "VERSION=" | grep -m1 -oE '[0-9]{1,2}([.][0-9]{1,2})([.][0-9]{1,2})')
+				printf 'Downloading latest version ($serverver) of $FILE\n'
+				downloaded_file "$DIR" "$FILE"
+				exit 0
+			;;
+			esac
+
   else
-    echo "Project Repository directory $DIR not found"
-    echo "Select the install option from the main menu to install the respository"
+	    echo "Project Repository directory $DIR not found"
+	    echo "Select the install option from the main menu to install the respository"
   fi
+done
+fi
 }
 
 remove_existing_installation() {
@@ -267,7 +239,7 @@ remove_existing_installation() {
     echo "Existing jq package found. Removing jq"
     if opkg remove jq; then echo "jq successfully removed"; else echo "Error occurred when removing jq"; fi
   else
-    echo "Unable to remove the jq package. Entware is not mounted"
+    echo "Entware package jq is not currentnly installed. Nothing to remove."
   fi
 
   # Remove entries from /jffs/scripts/init-start
@@ -297,6 +269,11 @@ remove_existing_installation() {
       printf '\n%b%s%b folder does not exist. No directory to remove\n' "$COLOR_GREEN" "$DIR" "$COLOR_WHITE"
     fi
   done
+
+ # unmount modified firmware code
+  umount /usr/sbin/vpnrouting.sh 2>/dev/null
+  umount /usr/sbin/updown.sh 2>/dev/null
+  umount /www/Advanced_OpenVPNClient_Content.asp 2>/dev/null
 
 }
 
@@ -361,8 +338,10 @@ download_file() {
   STATUS="$(curl --retry 3 -sL -w '%{http_code}' "$GITHUB_DIR/$FILE" -o "$DIR/$FILE")"
   if [ "$STATUS" -eq "200" ]; then
     printf '%b%s%b downloaded successfully\n' "$COLOR_GREEN" "$FILE" "$COLOR_WHITE"
-    if [ "$(echo "$FILE" | grep -cw '.sh')" -gt 0 ]; then
+    if [ "$(echo "$FILE" | grep -cw '.sh')" -eq 0 ]; then
       chmod 755 "$DIR/$FILE"
+    elif [ "$(echo "$FILE" | grep -cw '.asp')" -eq 0 ]; then
+      chmod 664 "$DIR/$FILE"
     fi
   else
     printf '%b%s%b download failed with curl error %s\n' "$COLOR_GREEN" "$FILE" "$COLOR_WHITE" "$STATUS"
@@ -411,7 +390,7 @@ init_start_update() {
 install_x3mRouting_LAN_clients() {
   create_project_directory
   download_file "$LOCAL_REPO" x3mRouting_client_nvram.sh
-  download_file "$LOCAL_REPO" x3mRouting_config.sh
+  download_file "$LOCAL_REPO" x3mRouting_client_config.sh
   download_file "$LOCAL_REPO" vpnrouting.sh
   download_file "$LOCAL_REPO" updown.sh
   download_file "$LOCAL_REPO" mount_files_lan.sh
@@ -474,20 +453,20 @@ update_installer() {
     printf '\n\nAn updated version of the x3mRouting menu as been detected\n'
     printf 'Updating the x3mRouting menu is highly recommended.\n'
     printf 'Would you like to download the new version now?\n'
-    echo "[1]  --> Yes"
-    echo "[2]  --> No"
+    printf '[%by%b] --> Yes\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
+    printf '[%bn%b] --> No\n' "${COLOR_GREEN}" "${COLOR_WHITE}"
     echo
-    printf "[1-2]: "
+    printf '\n%bOption ==>%b ' "${COLOR_GREEN}" "${COLOR_WHITE}"
     read -r "menu_update_installer"
     echo
     case "$menu_update_installer" in
-    1)
+    y)
       download_file /jffs/scripts install_x3mRouting.sh
       printf '\nUpdate Complete! %s\n' "$remotemd5"
       welcome_message
       break
       ;;
-    2)
+    n)
       Main_Menu
       break
       ;;

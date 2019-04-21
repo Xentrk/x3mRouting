934f54dbdae024dcd4ba88560eea7cf8d564dec0
Xentrk <xentrk@x3mtek.com>
2019-04-21 08:22:44 +0700

    updated output file name

--- x3mRouting_client_config.sh
+++ x3mRouting_client_config.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 ####################################################################################################
-# Script: x3mRouting_rules.sh
+# Script: x3mRouting_client_config.sh
 # VERSION=1.0.0
 # Author: Xentrk
-# 29-July-2018
+# 21-April-2019
 #
 #####################################################################################################
 # Description:
@@ -20,30 +20,30 @@
 logger -t "($(basename "$0"))" $$ Starting Script Execution
 
 # Uncomment the line below for debugging
-set -x
+#set -x
 
 VERSION="1.0"
-GITHUB_URL="https://github.com/Xentrk/Asuswrt-Merlin-Selective-Routing"
-GIT_REPO="Asuswrt-Merlin-Selective-Routing"
+GITHUB_URL="https://github.com/Xentrk/x3mRouting"
+CONFIG_FILE="/jffs/configs/x3mRouting_client_rules"
 COLOR_WHITE='\033[0m'
 COLOR_GREEN='\e[0;32m'
 
 printf '\n'
-printf '#############################################################################################################\n'
-printf '##                                                                                                         ##\n'
-printf '##  Welcome to the %bAsuswrt-Merlin-Selective-Routing%b configuration script                                   ##\n' "$COLOR_GREEN" "$COLOR_WHITE"
-printf '##  Version %s by Xentrk                                                                                  ##\n' "$VERSION"
-printf '##                                                                                                         ##\n'
-printf '##  Configuration instuctions available at %b%s%b      ##\n' "$COLOR_GREEN" "$GITHUB_URL" "$COLOR_WHITE"
-printf '##         ____        _         _                                                                         ##\n'
-printf '##        |__  |      | |       | |                                                                        ##\n'
-printf '##  __  __  _| |_ _ _ | |_  ___ | | __    ____ ____  _ _ _                                                 ##\n'
-printf '##  \ \/ / |_  | %b %b \  __|/ _ \| |/ /   /  _//    \| %b %b \                                                ##\n' "\`" "\`" "\`" "\`"
-printf '##   /  /  __| | | | |  |_ | __/|   <   (  (_ | [] || | | |                                                ##\n'
-printf '##  /_/\_\|___ |_|_|_|\___|\___||_|\_\[] \___\\\____/|_|_|_|                                                ##\n'
-printf '##                                                                                                         ##\n'
-printf '#############################################################################################################\n'
-printf '\n'
+printf '_______________________________________________________________\n'
+printf '|                                                             |\n'
+printf '|  Welcome to the %bx3mRouting%b LAN Client Configuration Script  |\n' "$COLOR_GREEN" "$COLOR_WHITE"
+printf '|  Version %b%s%b by Xentrk                                      |\n' "$COLOR_GREEN" "$VERSION" "$COLOR_WHITE"
+printf '|                                                             |\n'
+printf '|           Configuration instuctions available at            |\n'
+printf '|            %b%s%b             |\n' "$COLOR_GREEN" "$GITHUB_URL" "$COLOR_WHITE"
+printf '|         ____        _         _                             |\n'
+printf '|        |__  |      | |       | |                            |\n'
+printf '|  __  __  _| |_ _ _ | |_  ___ | | __    ____ ____  _ _ _     |\n'
+printf '|  \ \/ / |_  | %b %b \  __|/ _ \| |/ /   /  _//    \| %b %b \    |\n' "\`" "\`" "\`" "\`"
+printf '|   /  /  __| | | | |  |_ | __/|   <   (  (_ | [] || | | |    |\n'
+printf '|  /_/\_\|___ |_|_|_|\___|\___||_|\_\[] \___\\\____/|_|_|_|    |\n'
+printf '|                                                             |\n'
+printf '|_____________________________________________________________|\n\n'
 
 # Retrieve Static DHCP assignments; remove < and > symbols and separate fields with a space.
 nvram get dhcp_staticlist | sed 's/<//;s/>/ /g;s/</ /g' >/tmp/my-scripts.$$
@@ -56,12 +56,13 @@ word_count=$(head -1 /tmp/my-scripts.$$ | wc -w)
 static_leases_count=$((word_count / 3))
 
 # Check to see if a prior x3mRouting_rules file exists. Make a backup if it does.
-if [ -s "/jffs/configs/x3mRouting_rules" ]; then
+if [ -s "$CONFIG_FILE" ]; then
   TIMESTAMP=$(date +"%Y-%m-%d-%H.%M.%S")
-  if ! mv /jffs/configs/x3mRouting_rules /jffs/configs/x3mRouting_rules."$TIMESTAMP"; then
-    printf '\nBackup of the prior %b/jffs/configs/x3mRouting_rules%b file could not be made\n' "$COLOR_GREEN" "$COLOR_WHITE"
+  if ! mv "$CONFIG_FILE" "$CONFIG_FILE"."$TIMESTAMP"; then
+    printf '\nBackup of the prior %b%s%b file could not be made.\n' "$COLOR_GREEN" "$CONFIG_FILE" "$COLOR_WHITE"
   else
-    printf '\nExisting %b/jffs/configs/x3mRouting_rules%b file found.  A backup of the existing %b/jffs/configs/x3mRouting_rules%b file was made\n' "$COLOR_GREEN" "$COLOR_WHITE" "$COLOR_GREEN" "$COLOR_WHITE"
+    printf '\nExisting %b%s%b file found.\n' "$COLOR_GREEN" "$CONFIG_FILE" "$COLOR_WHITE"
+    printf 'A backup of the existing file was made.\n'
   fi
 fi
 
@@ -71,36 +72,37 @@ IP=2
 CLIENT=3
 
 # write a new x3mRouting_rules file
-true > /jffs/configs/x3mRouting_rules
+true >"$CONFIG_FILE"
 
 # write Static DHCP LAN Clients to /jffs/scripts/x3mRouting_rules
 while [ "$loop_count" -le "$static_leases_count" ]; do
-  cut -d' ' -f$IP,$CLIENT </tmp/my-scripts.$$ >>/jffs/configs/x3mRouting_rules
+  cut -d' ' -f"$IP","$CLIENT" </tmp/my-scripts.$$ >>"$CONFIG_FILE"
   IP=$((IP + 3))
   CLIENT=$((CLIENT + 3))
   loop_count=$((loop_count + 1))
 done
 
-sort /jffs/configs/x3mRouting_rules -o /jffs/configs/x3mRouting_rules
+sort "$CONFIG_FILE" -o "$CONFIG_FILE"
 
 rm /tmp/my-scripts.$$
 
 # Default all lan clients to OVPNC1 interface
-sed -i -e 's/^/1 /' /jffs/configs/x3mRouting_rules
+sed -i -e 's/^/1 /' "$CONFIG_FILE"
 
 # Add instructions at the top of the file
-sed -i '1s/^/#########################################################\n/' /jffs/configs/x3mRouting_rules
-sed -i '1s/^/# 5 = OVPNC5                                            #\n/' /jffs/configs/x3mRouting_rules
-sed -i '1s/^/# 4 = OVPNC4                                            #\n/' /jffs/configs/x3mRouting_rules
-sed -i '1s/^/# 3 = OVPNC3                                            #\n/' /jffs/configs/x3mRouting_rules
-sed -i '1s/^/# 2 = OVPNC2                                            #\n/' /jffs/configs/x3mRouting_rules
-sed -i '1s/^/# 1 = OVPNC1                                            #\n/' /jffs/configs/x3mRouting_rules
-sed -i '1s/^/# 0 = WAN                                               #\n/' /jffs/configs/x3mRouting_rules
-sed -i '1s/^/# the appropriate interface number in the first column  #\n/' /jffs/configs/x3mRouting_rules
-sed -i '1s/^/# Assign the interface for each LAN client by entering  #\n/' /jffs/configs/x3mRouting_rules
-sed -i '1s/^/#########################################################\n/' /jffs/configs/x3mRouting_rules
+sed -i '1s/^/#########################################################\n/' "$CONFIG_FILE"
+sed -i '1s/^/# 5 = OVPNC5                                            #\n/' "$CONFIG_FILE"
+sed -i '1s/^/# 4 = OVPNC4                                            #\n/' "$CONFIG_FILE"
+sed -i '1s/^/# 3 = OVPNC3                                            #\n/' "$CONFIG_FILE"
+sed -i '1s/^/# 2 = OVPNC2                                            #\n/' "$CONFIG_FILE"
+sed -i '1s/^/# 1 = OVPNC1                                            #\n/' "$CONFIG_FILE"
+sed -i '1s/^/# 0 = WAN                                               #\n/' "$CONFIG_FILE"
+sed -i '1s/^/# the appropriate interface number in the first column  #\n/' "$CONFIG_FILE"
+sed -i '1s/^/# Assign the interface for each LAN client by entering  #\n/' "$CONFIG_FILE"
+sed -i '1s/^/#########################################################\n/' "$CONFIG_FILE"
 
-printf '\nYou must now edit %b/jffs/configs/x3mRouting_rules%b and assign the interface for each LAN client\n' "$COLOR_GREEN" "$COLOR_WHITE"
+printf '\nYou must now edit %b%s%b and\n' "$COLOR_GREEN" "$CONFIG_FILE" "$COLOR_WHITE"
+printf 'assign the interface for each LAN client.\n'
 printf '\n'
 
 logger -t "($(basename "$0"))" $$ Ending Script Execution

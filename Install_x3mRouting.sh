7475b4158b7305412ef24cfed7255dede5b02896
Xentrk <xentrk@x3mtek.com>
2020-05-06 14:15:21 +0700

    enable debug mode for testing

--- Install_x3mRouting.sh
+++ Install_x3mRouting.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+set -x
 mkdir -p /jffs/addons/x3mRouting && /usr/sbin/curl --retry 3 https://raw.githubusercontent.com/Xentrk/x3mRouting/x3mRouting-NG/x3mRouting_Menu.sh -o /jffs/addons/x3mRouting/x3mRouting_Menu.sh && chmod 755 /jffs/addons/x3mRouting/x3mRouting_Menu.sh && rm /opt/bin/x3mRouting 2>/dev/null && ln -s /jffs/addons/x3mRouting/x3mRouting_Menu.sh /opt/bin/x3mRouting && x3mRouting || ln -s /jffs/addons/x3mRouting/x3mRouting_Menu.sh /opt/bin/x3mRouting && x3mRouting

25052a74df38f05cb4d80d9b24681f051293444c
maghuro <mrasquinho@gmail.com>
2020-05-06 15:52:37 +0100

    Remove comment and add downloading echo

--- Install_x3mRouting.sh
+++ Install_x3mRouting.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 while true; do
-# remove this line before final version mkdir -p /jffs/addons/x3mRouting && /usr/sbin/curl --retry 3 https://raw.githubusercontent.com/Xentrk/x3mRouting/x3mRouting-NG/x3mRouting_Menu.sh -o /jffs/addons/x3mRouting/x3mRouting_Menu.sh && chmod 755 /jffs/addons/x3mRouting/x3mRouting_Menu.sh && rm /opt/bin/x3mRouting 2>/dev/null && ln -s /jffs/addons/x3mRouting/x3mRouting_Menu.sh /opt/bin/x3mRouting && x3mRouting && exit || ln -s /jffs/addons/x3mRouting/x3mRouting_Menu.sh /opt/bin/x3mRouting && x3mRouting && exit
-
+  echo "Downloading, please wait patiently..."
   mkdir -p /jffs/addons/x3mRouting
   /usr/sbin/curl -s --retry 3 https://raw.githubusercontent.com/Xentrk/x3mRouting/x3mRouting-NG/x3mRouting_Menu.sh -o /jffs/addons/x3mRouting/x3mRouting_Menu.sh
   chmod 755 /jffs/addons/x3mRouting/x3mRouting_Menu.sh

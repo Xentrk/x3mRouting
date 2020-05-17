8a343dc0007cedc5edac7cda853bde9b988c823d
Xentrk <xentrk@x3mtek.com>
2020-05-17 15:57:01 +0700

    remove old symlinks if exist

--- Install_x3mRouting.sh
+++ Install_x3mRouting.sh
@@ -4,8 +4,12 @@ while true; do
   mkdir -p /jffs/addons/x3mRouting
   /usr/sbin/curl -s --retry 3 https://raw.githubusercontent.com/Xentrk/x3mRouting/x3mRouting-NG/x3mRouting_Menu.sh -o /jffs/addons/x3mRouting/x3mRouting_Menu.sh
   chmod 755 /jffs/addons/x3mRouting/x3mRouting_Menu.sh
+  # Remove old link if exist
+  if [ -d "/opt/bin" ] && [ "$(/opt/bin/find /opt/bin/ -maxdepth 1 -type l -ls | grep -c "/opt/bin/x3mRouting -> /jffs/addons/x3mRouting/x3mRouting_Menu.sh")" -eq 0 ]; then
+    rm /opt/bin/x3mRouting 2>/dev/null
+  fi
   # x3mRouting.sh Script
-  if [ -d "/opt/bin" ] && [ "$(find /opt/bin/ -maxdepth 1 -type l -ls | grep -c "/opt/bin/x3mRouting -> /jffs/addons/x3mRouting/x3mRouting_Menu.sh")" -eq 0 ]; then
+  if [ -d "/opt/bin" ] && [ "$(/opt/bin/find /opt/bin/ -maxdepth 1 -type l -ls | grep -c "/opt/bin/x3mRouting -> /jffs/addons/x3mRouting/x3mRouting_Menu.sh")" -eq 0 ]; then
     if [ -s "/opt/bin/x3mRouting" ]; then # old alias situation
       rm /opt/bin/x3mRouting 2>/dev/null
     fi

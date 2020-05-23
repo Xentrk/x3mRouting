852680ebd2dc21055fe8caa553f44690c15f6321
Xentrk <xentrk@x3mtek.com>
2020-05-23 18:20:49 +0700

    OPT2 del fix with jq

--- x3mRouting_Menu.sh
+++ x3mRouting_Menu.sh
@@ -225,7 +225,14 @@ Remove_OPT2() {
       1)
         # Remove the jq package
         Chk_Entware jq 1
-        [ "$READY" -eq "0" ] && "Existing jq package found. Removing jq" && opkg remove jq && echo "jq successfully removed" || echo "Error occurred when removing jq"
+        if [ "$READY" -eq "0" ]; then
+          echo "Existing jq package found. Removing jq"
+          if opkg remove jq; then
+            echo "jq successfully removed"
+          else
+            echo "Error occurred when removing jq"
+          fi
+        fi
         [ -s "$LOCAL_REPO/x3mRouting.sh" ] && rm -f "$LOCAL_REPO/x3mRouting.sh" && printf '\n%s%b%s%b%s\n' "Removal of " "$COLOR_GREEN" "$LOCAL_REPO/mount_files_lan.sh" "$COLOR_WHITE" " completed"
         [ -s "$LOCAL_REPO/openvpn-event.sh" ] && rm -f "$LOCAL_REPO/openvpn-event" && printf '\n%s%b%s%b%s\n' "Removal of " "$COLOR_GREEN" "$LOCAL_REPO/mount_files_lan.sh" "$COLOR_WHITE" " completed"
         break

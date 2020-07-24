abea7239098c00cbe9d336f727feaf213e081c45
Xentrk <xentrk@x3mtek.com>
2020-07-24 13:10:55 +0700

    add script to bounce vpn clients after firewall-event

---
+++ x3mRouting_firewall_start.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+####################################################################################################
+# Script: x3mRouting_firewall_start.sh
+# VERSION=1.0.0
+# Author: Xentrk
+# 24-July-2020
+#
+#####################################################################################################
+# Description:
+#
+# Applies to those who use the modified x3mRouting OpenVPN Client Screen. This script is called from
+# /jffs/scipts/firewall-start.
+#
+# A firewall restart will wipe the Accept DNS configuration = Exclusive rules and the PREROUTING rules
+# for IPSET lists. Bouncing the active VPN Clients after a firewalll restart will result in
+# vpnrouting.sh and updown-client.sh to reinstate the rules.
+#
+#####################################################################################################
+
+for VPN_CLIENT in 1 2 3 4 5; do
+  CLIENT_STATE=$(nvram get vpn_client${VPN_CLIENT}_state)
+  case "$CLIENT_STATE" in
+  2)
+    service restart_vpnclient${VPN_CLIENT}
+    logger -st "($(basename "$0"))" $$ Restarted VPN Client "$VPN_CLIENT"
+    ;;
+  esac
+done
